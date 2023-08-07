4b41e52b58c2bdd40cb5cae7ae98c0b914569fdd
Jack Stockwell <95311730+JackStockwell@users.noreply.github.com>
2023-08-07 22:11:55 +0100

    added models

---
+++ models/Reaction.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = (
+    {
+        reactionid: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => formatDate(timestamp)
+        }
+    },
+    {
+
+    }
+)
+
+module.exports = reactionSchema
---
+++ models/User.js
@@ -0,0 +1,53 @@
+const { Schema, model } = require('mongoose');
+
+const validEmail = (email) => {
+    const reg = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
+    return reg.test(email)
+}
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trimmed: true,
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            validate: [
+                validEmail,
+                "You must enter a valid email!"
+            ]
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+    }
+);
+
+userSchema
+    .virtual('friendCount')
+    .get(function () {
+        return this.friends.length;
+    })
+
+const User = model('user', userSchema);
+
+module.exports = User;
