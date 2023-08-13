a95ca3ce39fc1c3c5e977bcd80f88e7d3947e764
Jack Stockwell <95311730+JackStockwell@users.noreply.github.com>
2023-08-13 20:27:45 +0100

    added friend follow routes.

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -5,5 +5,11 @@ const userController = require('../../controllers/user-controller')
 router
     .route('/')
         .get(userController.getUsers)
+        .post(userController.newUser);
+router
+    .route('/:id')
+        .get(userController.getUserSingle)
+        .put(userController.updateUser)
+        .delete(userController.deleteUser);
 
 module.exports = router;
