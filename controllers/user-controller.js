95836d1234434ae2c77b4d73e9c89e0c9be3aff0
Jack Stockwell <95311730+JackStockwell@users.noreply.github.com>
2023-08-13 19:13:41 +0100

    started user controller, fixed thought controller not returning

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,18 +1,142 @@
 const { Thought, User, } = require('../models')
 
 const userController = {
+    // Finds all users
+    // GET '/'
     async getUsers (req, res) {
         try {
+            // Finds all users
             const userData = await User.find({})
                 .select('-__v')
                 .populate('thoughts')
 
             res.status(200).json(userData)
+            return
         } catch (err) {
             console.log(err)
             res.status(500).json(err)
         }
     },
+
+    // Get a single user.
+    // GET '/:id'
+    async getUserSingle (req, res) {
+        try {
+
+            const userData = await User.findOne(
+                {_id: req.params}
+            )
+
+            // Validation check, ensures that a valid id was found/parsed.
+            if (!userData) {
+                res
+                    .status(404)
+                    .json({
+                        message: "No user found with that ID!"
+                    })
+                return
+            }
+
+            res.status(200).json(userData)
+            return
+
+        } catch (err) {
+            console.error(err)
+            res.status(500).json(err)
+        }
+    },
+
+    // Create a new user
+    // POST '/'
+    async newUser (req, res) {
+        try {
+
+            // Creates a new user.
+            const userData = await User.create(req.body)
+
+            // Validation check, ensures that a valid id was found/parsed.
+            if (!userData) {
+                res
+                    .status(404)
+                    .json({
+                        message: "Incorrect/missing data! Ensure an Email and Username are set."
+                    })
+                return
+            }
+
+            res.status(200).json(userData)
+            return
+
+        } catch (err) {
+            console.error(err)
+            res.status(500).json(err)
+        }
+    },
+
+    // Update a user's details.
+    // PUT '/:id'
+    async updateUser (req, res) {
+        try {
+            
+            // Finds the user, updates their infomation.
+            const userData = await User.findOneAndUpdate(
+                {_id: req.body.id},
+                {$set: req.body},
+                {runValidators: true, new: true}
+            )
+
+            if (!userData) {
+                res
+                    .status(404)
+                    .json({
+                        message: "No user found with that ID!"
+                    })
+                return
+            }
+
+        } catch (err) {
+            console.error(err)
+            res.status(500).json(err)
+        }
+    },
+
+    // Delete a user and their thoughts.
+    // DELETE '/:id'
+    async deleteUser (req, res) {
+        try {
+
+            const userData = await User.findOne(
+                {_id: req.params.id}
+            )
+
+            if (!userData) {
+                res
+                    .status(404)
+                    .json({
+                        message: "No user found with that ID!"
+                    })
+                return
+            }
+
+            await Thought.deleteMany(
+                {username: userData.username}
+            )
+
+            await User.deleteOne(
+                {_id: req.params.id}
+            )
+
+            res
+                .status(200)
+                .json({
+                    message: "User and their thoughts forgotten from the Database!"
+                })
+            return
+        } catch (err) {
+            console.error(err)
+            res.status(500).json(err)
+        }
+    }
 }
 
 module.exports = userController;
